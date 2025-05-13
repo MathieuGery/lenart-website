2b1cf9dec541a3b727007b62216687d0c411f78b
MathieuGery <mathieu.gery@epitech.eu>
2025-05-13 21:47:24 +0200

    add tarifs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -98,13 +98,13 @@ export function Footer() {
   return (
     <Container as="footer" className="mt-24 w-full sm:mt-32 lg:mt-40">
       <FadeIn>
-        <div className="grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2  border-t border-neutral-950/10 pt-12">
           <Navigation />
           <div className="flex lg:justify-end">
             <NewsletterForm />
           </div>
         </div>
-        <div className="mb-20 mt-24 flex flex-wrap items-end justify-between gap-x-6 gap-y-4 border-t border-neutral-950/10 pt-12">
+        <div className="mb-20 mt-24 flex flex-wrap items-end justify-between gap-x-6 gap-y-4">
           <Link href="/" aria-label="Home">
             <Logo className="h-8" fillOnHover />
           </Link>
